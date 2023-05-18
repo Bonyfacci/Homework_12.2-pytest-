c5c8269231460680040bb87c4722c56f28d362ed
Сергей Платонов <platonovpochta@gmail.com>
2023-05-18 22:17:00 +0200

    add tests - 100% (with fixtures)

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,16 +3,22 @@ import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.mark.parametrize('array, index, default, expected', [
+    ([1, 2, 3], 1, "test", 2),
+    ([1, 2, 3], -1, "test", "test")
+])
+def test_get(array, index, default, expected):
+    assert arrs.get(array, index, default) == expected
     with pytest.raises(IndexError):
         arrs.get([], 0, "test") == "test"
-    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3], -1) == [3]
-    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
+@pytest.mark.parametrize('coll, start, end, expected', [
+    ([1, 2, 3, 4], 1, 3, [2, 3]),
+    ([1, 2, 3], 1, None, [2, 3]),
+    ([], 1, None, []),
+    ([1, 2, 3], -1, None, [3]),
+    ([1, 2, 3], -4, None, [1, 2, 3])
+])
+def test_slice(coll, start, end, expected):
+    assert arrs.my_slice(coll, start, end) == expected
